205363b74a0c984eaa17f1256d554f25d1f5c0cc
Alex Luh <aluh@mit.edu>
2015-12-24 17:46:14 -0800

    Insert temp user data on home page

--- views/controllers/homeController.js
+++ views/controllers/homeController.js
@@ -1,4 +1,13 @@
 angryCartels.controller('homeController', function($scope) {
 	// create a message to display in our view
 	$scope.message = 'Enjoy the fun game!';
+	$scope.username = "testuser";
+	$scope.name = "Test User";
+	$scope.stats = {
+		gamesPlayed: 0,
+		wins: 0,
+		losses:0
+	};
+	$scope.friendList = [];
+	$scope.friends = 0;
 });
