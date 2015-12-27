194f42d1fb38d630c84a3e6f67ff59df4d7b753a
Alex Luh <aluh@mit.edu>
2015-12-27 00:09:09 -0800

    Start adding Community Chest cards

---
+++ routes/communityChest.js
@@ -0,0 +1,78 @@
+var cards = [
+    {
+        "title": "Insurance Premiums Due",
+        "description": "Pay $50 to the Pool.",
+        "play": "immediately",
+        "short": "pay pool 50"
+    },
+    {
+        "title": "You Inherit $100",
+        "description": "Collect $100 from the Bank.",
+        "play": "immediately",
+        "short": "collect 100"
+    },
+    {
+        "title": "Just Say \"NO\"!",
+        "description": "Play at any time to stop another player's action against you.",
+        "play": "keep",
+        "short": "just say no"
+    },
+    {
+        "title": "Doctor's Fee",
+        "description": "Pay $50 to the Pool.",
+        "play": "immediately",
+        "short": "pay pool 50"
+    },
+    {
+        "title": "Opening Night Tickets!",
+        "description": "You won tickets to the hottest show in town.\nSell them to each player for $50.\n(Collect $50 from each player.)",
+        "play": "immediately",
+        "short": "collect 50 from each player"
+    },
+    {
+        "title": "Happy Birthday!",
+        "description": "Collect $10 from each player, and move to the\nBirthday Gift space and follow the instructions.",
+        "play": "immediately",
+        "short": "collect 10 from each player"
+    },
+    {
+        "title": "Business Trip",
+        "description": "Draw 2 Travel Vouchers from the deck.",
+        "play": "immediately",
+        "short": "business trip"
+    },
+    {
+        "title": "April 15, Taxes Due!",
+        "description": "Move directly to Income Tax, (do not pass\nany Pay Corner, do not collect any money),\nand pay the fine - OR - go directly to Jail.",
+        "play": "immediately",
+        "short": "income tax"
+    },
+    {
+        "title": "Sale of Stock Bonus",
+        "description": "You may sell any one stock back to the\nbank for its value plus $50.",
+        "play": "keep",
+        "short": "sell stock"
+    },
+    {
+        "title": "Just Say \"NO\"!",
+        "description": "Play at any time to stop another player's action against you.",
+        "play": "keep",
+        "short": "just say no"
+    },
+    {
+        "title": "Game Night!",
+        "description": "Pick an opponent. Both roll. Highest-\nroller collects $200 from the Bank.",
+        "play": "immediately",
+        "short": "game night"
+    },
+    {
+        "title": "A Moving Experience",
+        "description": "Move to Any Transportation Property (Railroad\nor Cab Co.). If Unowned, you may purchase it\nor put it up for auction. If Owned, pay rent.",
+        "play": "immediately",
+        "short": "move transportation"
+    },
+];
+
+var communityChest = {};
+
+module.exports = communityChest;
