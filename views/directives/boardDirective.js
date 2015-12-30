a12073ed3c4289c3d2b4fbfc3fd8ca93bf1b4a22
Alex Luh <aluh@mit.edu>
2015-12-29 16:41:10 -0800

    Add temp functions to get mouse coords on canvas

--- views/directives/boardDirective.js
+++ views/directives/boardDirective.js
@@ -3,7 +3,7 @@ angryCartels.directive('board', function($window) {
         restrict: "EA",
         controller: "boardController",
         templateUrl: '/pages/board.html',
-        link: function(scope, elementm, attrs) {
+        link: function(scope, element, attrs) {
             var canvas = document.getElementById('testCanvas');
             var ctx = canvas.getContext('2d');
             var startX = 0;
@@ -21,6 +21,21 @@ angryCartels.directive('board', function($window) {
             };
             // Set source path
             img.src = 'img/game_board.jpg';
+            // Functions for getting and displaying mouse coordinates over canvas
+            function getMousePosition(canvas, event) {
+                var rectangle = canvas.getBoundingClientRect();
+                return {
+                    x: event.clientX - rectangle.left,
+                    y: event.clientY - rectangle.top
+                };
+            }
+            canvas.addEventListener('mousemove', function(event) {
+                var mousePosition = getMousePosition(canvas, event);
+                scope.$apply(function() {
+                    scope.mouseX = mousePosition.x;
+                    scope.mouseY = mousePosition.y;
+                });
+            });
         }
     }
 });
