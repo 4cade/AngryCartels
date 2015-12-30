5ed89378c68b438a7443419d509f2bb79a91302d
Alex Luh <aluh@mit.edu>
2015-12-29 21:23:12 -0800

    Test drawing 8 shapes same location in game board

--- views/directives/boardDirective.js
+++ views/directives/boardDirective.js
@@ -21,6 +21,27 @@ angryCartels.directive('board', function($window) {
             };
             // Set source path
             img.src = 'img/game_board.jpg';
+            // Draw 8 player icons on the starting location
+            setTimeout(function () {
+                // Temporarily use rectangles to represent players
+                ctx.fillStyle = "#0000FF"; //blue
+                ctx.fillRect(812, 812, 6, 6);
+                ctx.fillStyle = "#800080"; //purple
+                ctx.fillRect(821, 812, 6, 6);
+                ctx.fillStyle = "#008000"; //green
+                ctx.fillRect(830, 812, 6, 6);
+                ctx.fillStyle = "#FF00FF"; //magenta
+                ctx.fillRect(812, 821, 6, 6);
+                ctx.fillStyle = "#000000"; //black
+                ctx.fillRect(821, 821, 6, 6);
+                ctx.fillStyle = "#FFA500"; //orange
+                ctx.fillRect(830, 821, 6, 6);
+                ctx.fillStyle = "#6F4E37"; //red
+                ctx.fillRect(812, 830, 6, 6);
+                ctx.fillStyle = "#FF0000"; //coffee brown
+                ctx.fillRect(821, 830, 6, 6);
+                console.log("The rectangles were drawn!");
+            }, 500);
         }
     }
 });
