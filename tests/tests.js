5e448b4fd38f2466281e0005d206db1c1600e406
Alex Luh <aluh@mit.edu>
2016-01-02 20:19:45 -0800

    Remove sample files and fix QUnit test template

--- tests/tests.js
+++ tests/tests.js
@@ -1,101 +1,17 @@
-// Create game object and fake gameData here
-// var testGame = {};
-// var testGameData = {};
-
+// Create gamePresets data up here
+// var gamePresets = {};
 
 // Example QUnit test
 QUnit.test("hello test", function(assert) {
     assert.ok(1 == "1", "Passed!");
 });
 
-// Sample test using function in ../routes/sample.js
-// Test passes because function defined and not inside object
-QUnit.test("Test calcSum function", function(assert) {
-    var pospos = calcSum(7, 18);
-    var posneg = calcSum(16, -4);
-    var negneg = calcSum(-1, -20);
-    assert.ok(pospos === 25, "Correctly calculated sum of two positive numbers.");
-    assert.ok(posneg === 12, "Correctly calculated sum of a positive and negative number.");
-    assert.ok(negneg === -21, "Correctly calculated sum of two negative numbers.");
-});
-
-// Sample test using functions in ../routes/sample.js
-// Note that the game variable used here is the one created inside samples.js
-// So if we change the value of players in the game object in samples.js, we will fail this test
-// This test passes
-QUnit.test("sample test", function(assert) {
-    var p1 = game.addPlayer();
-    var p2 = add5Players();
-    assert.ok(p1 === 1, "worked");
-    assert.ok(p2 === 6, "worked");
-});
-
-// Sample tests using functions in ../routes/sample.js
-// Test failed: game.addPlayer is not a function
-// Object method not properly created inside an object
-QUnit.test("sample test 2", function(assert) {
-    var testGame = {players: 100};
-    var p3 = testGame.addPlayer();
-    var p4 = add5Players();
-    assert.ok(p3 === 101, "worked");
-    assert.ok(p4 === 106, "worked");
-});
-
-// Sample test using functions in ../routes/sample2.js
-// This test passes
-// Object method correctly defined inside object
-QUnit.test("Person constructor test", function(assert) {
-    var person = new Person(20);
-    var age = person.getAge();
-    assert.ok(age === 20, "worked");
-});
-
-// Sample test using function in ../routes/sample2.js
-// This test passes
-// Object method correctly defind inside object
-QUnit.test("Person birthday function", function(assert) {
-    var peep = new Person(17);
-    var newAge = peep.birthday();
-    assert.ok(newAge === 18, "worked");
-});
-
-// Sample test using function in ../routes/sample2.js
-// Test failed: calcMult is not defined
-// As expected, this method wasn't attached to the object when created in sample2.js, and is not testable
-QUnit.test("test calcMult function", function(assert) {
-    var result = calcMult(3, 7);
-    assert.ok(result === 21, "worked");
-});
-
-// Sample test using function in ../routes/sample3.js
-// This test passes
-// Object method correctly defined inside object
-QUnit.test("Student constructor test", function(assert) {
-    var student = new Student(4);
-    var classes = student.getNumClasses();
-    assert.ok(classes === 4, "worked");
-});
-
-// Sample test using function in ../routes/sample3.js
-// This test passes
-// Object method correctly defined inside object
-QUnit.test("Student addClass function", function(assert) {
-    var boss = new Student(5);
-    var moreClass = boss.addClass();
-    assert.ok(moreClass === 6, "worked");
-});
-
-// Sample test using function in ../routes/sample3.js
-// Test failed: calcDiff is not defined
-// As expected, this method wasn't attached to the object when created in sample3.js, and is not testable
-QUnit.test("test calcDiff function", function(assert) {
-    var result = calcDiff(20, 6);
-    assert.ok(result === 14, "worked");
-});
-
 // Check that game was initialized correctly
-QUnit.test("Test initializeBoard", function(assert) {
-    // assuming you've changed the initializeBoard function to return a new gameData object?
-    // var initializedGameData = testGame.initializeBoard(testGameData);
-    assert.ok(true == true, "Checked that some property of the game data was initialized correctly.")
+QUnit.test("Test game data initialization", function(assert) {
+    // var testGame = new Game(gamePresets);
+    // assert.ok(testGame.gameData["houses"] === 81, "house preset correct");
+    // assert.ok(testGame.gameData["hotels"] === 31, "hotel preset correct");
+    // assert.ok(testGame.gameData["skyscrapers"] === 16, "skyscraper preset correct");
+    // assert.ok(testGame.gameData["recentLocation"] === "go", "every player starts at GO");
+    // assert.ok(observed value == expected value, "Comment on what the assertion checks");
 });
