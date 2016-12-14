8b94915b68e6d82b2130c253728d37fc19aca4ea
Leif Myer <leifmyer@gmail.com>
2016-12-13 16:22:38 -0800

    added temp card click and scale wip

---
+++ AngryCartels/Assets/Scripts/UI Scripts/PlayerCardSelected.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCardSelected : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void CardSelected()
+    {
+        Debug.Log("TODO: card selected~");
+    }
+}
