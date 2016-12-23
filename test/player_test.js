94e33698972e82f9a9d019121d462cd66ed6eb51
Zachery Miranda <zmiranda@mit.edu>
2016-12-23 12:45:13 -0800

    more player tests

--- test/player_test.js
+++ test/player_test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const Player = require('../backend/player')
+const Player = require('../backend/player');
 
 // tests the Player object
 describe('Player', function() {
@@ -11,10 +11,6 @@ describe('Player', function() {
 			assert.equal(player1.name, 'Bob');
 			assert.equal(player1.money, 3200);
 		})
-	});
-
-	describe("#propertyTests", function() {
-		// TODO
 	});
 
 	describe('#moneyTests', function() {
@@ -58,16 +54,88 @@ describe('Player', function() {
 	});
 
 	describe("#busPassTests", function() {
-		// TODO
+		it("should add all of the bus passes", function() {
+			const expected = {"forward any": 2, "derp 2": 1, "back 3": 1}
+			player1.gainBusPass('forward any');
+			player1.gainBusPass('derp 2');
+			player1.gainBusPass('back 3');
+			player1.gainBusPass('forward any');
+			assert.deepEqual(expected, player1.busTickets)
+		});
+
+		it("should remove all of the bus passes", function() {
+			const expected = {}
+			player1.useBusPass('forward any');
+			player1.useBusPass('derp 2');
+			player1.useBusPass('back 3');
+			player1.useBusPass('forward any');
+			assert.deepEqual(expected, player1.busTickets)
+		});
+
+		it("should add and remove the bus passes", function() {
+			const expected = {"forward any": 1, "derp 2": 1}
+			player1.gainBusPass('forward any');
+			player1.gainBusPass('derp 2');
+			player1.gainBusPass('back 3');
+			player1.gainBusPass('forward any');
+			player1.useBusPass('back 3');
+			player1.useBusPass('forward any');
+			assert.deepEqual(expected, player1.busTickets)
+		});
+
+		it("should have all of the tickets expire", function() {
+			const expected = {"forward expire": 1}
+			player1.gainBusPass('forward any');
+			player1.gainBusPass('derp 2');
+			player1.gainBusPass('back 3');
+			player1.gainBusPass('forward any');
+			player1.useBusPass('back 3');
+			player1.gainBusPass('forward expire');
+			assert.deepEqual(expected, player1.busTickets);
+			player1.gainBusPass('forward expire');
+			assert.deepEqual(expected, player1.busTickets)
+		});
 	});
 
 	describe("#specialCardTests", function() {
-		// TODO
+		it("should add all of the special cards", function() {
+			player1.gainSpecialCard('just say no');
+			player1.gainSpecialCard('get out of jail free');
+			player1.gainSpecialCard('steal');
+			player1.gainSpecialCard('just say no');
+			assert(player1.specialCards.indexOf('just say no') > -1);
+			assert(player1.specialCards.indexOf('steal') > -1);
+			assert(player1.specialCards.indexOf('get out of jail free') > -1);
+			assert.equal(4, player1.specialCards.length);
+		});
+
+		it("should remove all of the special cards", function() {
+			player1.useSpecialCard('just say no');
+			player1.useSpecialCard('get out of jail free');
+			player1.useSpecialCard('steal');
+			player1.useSpecialCard('just say no');
+			assert.equal(-1, player1.specialCards.indexOf('just say no'));
+			assert.equal(-1, player1.specialCards.indexOf('steal'));
+			assert.equal(-1, player1.specialCards.indexOf('get out of jail free'));
+			assert.equal(0, player1.specialCards.length);
+		});
 	});
 
-	describe("#locationTests", function() {
-		// TODO
+	describe("#locationTest", function() {
+		it("should move the player to the location with side effects", function() {
+			let moveInfo = {"moneyGained": 299, "currentLocation": 'bob', "movedTo": []};
+			player1.moveToLocation(moveInfo);
+			assert.equal("bob", player1.location);
+			assert.equal(300, player1.money);
+
+			moveInfo["moneyGained"] = 0
+			moveInfo["currentLocation"] = "dude";
+			assert.equal("dude", player1.location);
+			assert.equal(300, player1.money);
+		});
 	});
 
-	// TODO more tests
+	describe("#propertyTests", function() {
+		// TODO when Properties are tested to be correct, also test getNetWorth() inside this
+	});
 })
