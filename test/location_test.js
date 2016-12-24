060b8e11cd269e83d8bb42d58466ddbd0f3be518
Zachery Miranda <zmiranda@mit.edu>
2016-12-23 16:08:32 -0800

    wrote millions of tests

--- test/location_test.js
+++ test/location_test.js
@@ -1,10 +1,272 @@
 const assert = require('assert');
 const Place = require('../backend/location/place');
+const Property = require('../backend/location/property');
 const HouseProperty = require('../backend/location/houseProperty');
 const Utility = require('../backend/location/utility');
 const Railroad = require('../backend/location/railroad');
 
 // tests the Location objects
 describe('Location', function() {
+    describe("#placeTest", function() {
+        it("should parse a normal location correctly", function() {
+            const name = "birthday gift"
+            const spot = {
+                            "type": "spot",
+                            "forward": ["mulholland dr"],
+                            "backward": ["ute cab co"],
+                            "side": [2],
+                            "track": [2],
+                            "above": ["park pl"],
+                        }
+            const place = new Place(name, spot);
+            assert.equal(place.name, name);
+            assert.equal(place.kind, spot.type);
+            assert.deepEqual(place.forward, spot.forward);
+            assert.deepEqual(place.backward, spot.backward);
+            assert.deepEqual(place.side, spot.side);
+            assert.deepEqual(place.above, spot.above);
+        })
+    });
 
+    describe("#propertyTests", function() {
+        const name = "electric company";
+        const prop = {
+                        "type": "utility",
+                        "quality": "utility",
+                        "rent": [10, 20, 40, 80, 100, 120, 150],
+                        "mortgage": 75,
+                        "forward": ["states ave"],
+                        "backward": ["st charles pl"],
+                        "side": [1],
+                        "track": [1],
+                        "above": ["bonus"],
+                        "below": ["internet service provider"]
+                     };
+        const property = new Property(name, prop);
+
+        it("should initialize a Property correctly", function() {
+            assert.equal(property.kind, prop.type);
+            assert.equal(property.mortgage, prop.mortgage);
+            assert.equal(property.mortgage, prop.forward);
+            assert.equal(property.below, prop.below);
+            assert.equal(property.rent, prop.rent);
+            assert.equal(property.color, prop.quality);
+            assert.equal(property.cost, 2*prop.mortgage);
+        });
+
+        it("should have the cost be the value", function() {
+            assert.equal(property.getValue(), property.cost);
+        });
+
+        it("should get an owner", function() {
+            assert.equal(property.owner, null);
+            property.setOwner("Bob");
+            assert.equal(property.owner, "Bob");
+        });
+
+        it("should be mortgaged", function() {
+            property.mortgage();
+            assert(property.isMortgaged);
+            assert.equal(property.getValue(), prop.mortgage);
+        });
+
+        it("should be unmortgaged", function() {
+            property.unmortgage()
+            assert(!property.isMortgaged);
+            assert.equal(property.getValue(), 2*prop.mortgage);
+        });
+    });
+
+    describe("#housePropertyTests", function() {
+        const name = "newbury st";
+        const prop = {
+                        "type": "property",
+                        "quality": "black",
+                        "rent": [40, 185, 550, 1200, 1500, 1700, 2700],
+                        "mortgage": 190,
+                        "house": 200,
+                        "forward": ["pennsylvania railroad"],
+                        "backward": ["boylston st"],
+                        "side": [1],
+                        "track": [0],
+                        "below": ["virginia ave"]
+                     };
+
+        const property = HouseProperty(name, prop);
+
+        it("should initialize a HouseProperty correctly", function() {
+            assert.equal(property.kind, prop.type);
+            assert.equal(property.mortgage, prop.mortgage);
+            assert.equal(property.mortgage, prop.forward);
+            assert.equal(property.below, prop.below);
+            assert.equal(property.rent, prop.rent);
+            assert.equal(property.color, prop.quality);
+            assert.equal(property.cost, 2*prop.mortgage);
+            assert.equal(property.housePrice, prop.house);
+            assert.equal(property.houses, 0);
+            assert.equal(property.getValue(), property.cost);
+            assert.equal(property.getRent(false), prop.rent[0]);
+            assert.equal(property.getRent(true), 2*prop.rent[0]);
+        });
+
+        it("should add houses and increase rent/value", function() {
+            property.addHouse();
+            assert.equal(property.getRent(false), prop.rent[1]);
+            assert.equal(property.getRent(true), prop.rent[1]);
+            property.addHouse();
+            assert.equal(property.getRent(false), prop.rent[2]);
+            property.addHouse();
+            property.addHouse();
+            assert.equal(property.getValue(), 4*prop.house+2*prop.mortgage);
+            assert.equal(property.getRent(false), prop.rent[4]);
+            property.addHouse();
+            property.addHouse();
+            assert.equal(property.getRent(false), prop.rent[6]);
+            assert.equal(property.houses, 6)
+            property.addHouse(); // can't go above 6 houses (aka skyscraper)
+            assert.equal(property.houses, 6);
+            assert.equal(property.getRent(true), prop.rent[6]);
+            assert.equal(property.getValue(), 6*prop.house+2*prop.mortgage);
+        });
+
+        it("should remove houses and decrease rent/value", function() {
+            property.removeHouse();
+            assert.equal(property.getRent(false), prop.rent[5]);
+            assert.equal(property.getValue(), 5*prop.house+2*prop.mortgage);
+            property.removeHouse();
+            assert.equal(property.getRent(false), prop.rent[4]);
+            property.removeHouse();
+            property.removeHouse();
+            assert.equal(property.getRent(false), prop.rent[2]);
+            assert.equal(property.getValue(), 2*prop.house+2*prop.mortgage);
+            property.removeHouse();
+            property.removeHouse();
+            assert.equal(property.getRent(false), prop.rent[0]);
+            assert.equal(property.houses, 0)
+            property.removeHouse(); // can't go below 0 houses
+            assert.equal(property.houses, 0);
+            assert.equal(property.getValue(), 0*prop.house+2*prop.mortgage);
+        });
+
+        it("should not charge any rent or let you add houses if mortgaged", function() {
+            property.mortgage();
+            assert(property.isMortgaged);
+            assert.equal(property.getRent(false), 0);
+            property.addHouse();
+            assert.equal(property.houses, 0);
+
+            // now unmortgage and see if still works
+            property.unmortgage();
+            assert(!property.isMortgaged)
+            property.addHouse();
+            assert.equal(property.getRent(true), prop.rent[1]);
+        });
+    });
+
+    describe("#utilityTests", function() {
+        const name = "electric company";
+        const prop = {
+                        "type": "utility",
+                        "quality": "utility",
+                        "rent": [10, 20, 40, 80, 100, 120, 150],
+                        "mortgage": 75,
+                        "forward": ["states ave"],
+                        "backward": ["st charles pl"],
+                        "side": [1],
+                        "track": [1],
+                        "above": ["bonus"],
+                        "below": ["internet service provider"]
+                     };
+        const property = new Property(name, prop);
+
+        it("should initialize a Utility correctly", function() {
+            assert.equal(property.name, prop.name);
+            assert.equal(property.kind, prop.type);
+            assert.equal(property.mortgage, prop.mortgage);
+            assert.equal(property.mortgage, prop.forward);
+            assert.equal(property.below, prop.below);
+            assert.equal(property.rent, prop.rent);
+            assert.equal(property.color, prop.quality);
+            assert.equal(property.cost, 2*prop.mortgage);
+            assert.equal(property.getValue(), property.cost);
+        });
+
+        it("should charge the correct amount of rent", function() {
+            assert.equal(property.getRent(0), prop.rent[0]);
+            assert.equal(property.getRent(3), prop.rent[3]);
+            assert.equal(property.getRent(7), prop.rent[7]);
+            assert.equal(property.getRent(9), prop.rent[8]); // should just assume that 8 is max
+
+            // handle mortgage cases
+            property.mortgage();
+            assert.equal(property.getRent(4), 0);
+            property.unmortgage();
+            assert.equal(property.getRent(5), prop.rent[5]);
+        });
+    });
+
+    describe("#railroadTests", function() {
+        const name = "short line";
+        const spot = {
+                        "type": "transportation",
+                        "quality": "railroad",
+                        "rent": [25, 50, 100, 200],
+                        "mortgage": 100,
+                        "forward": ["chance middle east", "reverse"],
+                        "backward": ["pennsylvania ave", "biscayne ave"],
+                        "side": [3],
+                        "track": [1, 0],
+                        "below": ["sewage system"]
+                    };
+
+        const rr = new Railroad(name, spot);
+
+        it("should initialize a Railroad correctly", function() {
+            assert.equal(rr.name, spot.name);
+            assert.equal(rr.kind, spot.type);
+            assert.equal(rr.mortgage, spot.mortgage);
+            assert.equal(rr.mortgage, spot.forward);
+            assert.equal(rr.below, spot.below);
+            assert.equal(rr.rent, spot.rent);
+            assert.equal(rr.color, spot.quality);
+            assert.equal(rr.cost, 2*spot.mortgage);
+            assert.equal(rr.getValue(), rr.cost);
+            assert(!rr.hasTrainDepot);
+            assert.equal(rr.trainDepotPrice, 100);
+        });
+
+        it("should charge rent correctly", function() {
+            assert.equal(rr.getRent(0), spot.rent[0]); // just in case a typo comes from above
+            assert.equal(rr.getRent(1), spot.rent[0]);
+            assert.equal(rr.getRent(2), spot.rent[1]);
+            assert.equal(rr.getRent(4), spot.rent[3]);
+        });
+
+        it("should handle mortgages correctly", function() {
+            rr.mortgage();
+            assert.equal(rr.getRent(3), 0);
+            assert.equal(rr.getValue(), spot.mortgage);
+            rr.addTrainDepot();
+            assert.equal(rr.getRent(2), 0);
+            assert.equal(rr.getValue(), spot.mortgage);
+            rr.unmortgage();
+            assert.equal(rr.getValue(), 2*spot.mortgage);
+        })
+
+        it("should handle train depots correctly", function() {
+            rr.addTrainDepot();
+            assert.equal(rr.getRent(1), 2*spot.rent[0]);
+            assert.equal(rr.getRent(4), 2*spot.rent[3]);
+            assert.equal(rr.getValue(), 2*spot.mortgage+rr.trainDepotPrice);
+
+            rr.removeTrainDepot();
+            assert.equal(rr.getRent(3), spot.rent[2]);
+            assert.equal(rr.getValue(), 2*spot.mortgage);
+        });
+
+    });
+
+    describe("#taxiTests", function() {
+        // TODO
+    });
 });
--- test/player_test.js
+++ test/player_test.js
@@ -1,142 +1,198 @@
 const assert = require('assert');
 const Player = require('../backend/player');
+const HouseProperty = require('../backend/location/houseProperty');
 
 // tests the Player object
 describe('Player', function() {
 
-	const player1 = new Player('Bob', 'go', 1);
+    const player1 = new Player('Bob', 'go', 1);
 
-	describe("#initialize", function() {
-		it("should have all correct parts correctly initialized", function() {
-			assert.equal(player1.name, 'Bob');
-			assert.equal(player1.money, 3200);
-		})
-	});
+    describe("#initialize", function() {
+        it("should have all correct parts correctly initialized", function() {
+            assert.equal(player1.name, 'Bob');
+            assert.equal(player1.money, 3200);
+        })
+    });
 
-	describe('#moneyTests', function() {
-		it("should have added money", function() {
-			const hasMoney = player1.deltaMoney(200);
-			assert.equal(3400, player1.money);
-			assert.equal(true, hasMoney);
-		});
+    describe('#moneyTests', function() {
+        it("should have added money", function() {
+            const hasMoney = player1.deltaMoney(200);
+            assert.equal(player1.money, 3400);
+            assert.equal(hasMoney, true);
+        });
 
-		it("should have removed money", function() {
-			const hasMoney = player1.deltaMoney(-2100);
-			assert.equal(1300, player1.money);
-			assert.equal(true, hasMoney);
-		});
+        it("should have removed money", function() {
+            const hasMoney = player1.deltaMoney(-2100);
+            assert.equal(player1.money, 1300);
+            assert.equal(hasMoney, true);
+        });
 
-		it("should handle multiple transactions", function() {
-			player1.deltaMoney(-1100);
-			player1.deltaMoney(500);
-			player1.deltaMoney(-600);
-			player1.deltaMoney(900);
-			player1.deltaMoney(-50);
-			player1.deltaMoney(-250);
-			const hasMoney = player1.deltaMoney(-100);
-			assert.equal(600, player1.money);
-			assert.equal(true, hasMoney);
-		});
+        it("should handle multiple transactions", function() {
+            player1.deltaMoney(-1100);
+            player1.deltaMoney(500);
+            player1.deltaMoney(-600);
+            player1.deltaMoney(900);
+            player1.deltaMoney(-50);
+            player1.deltaMoney(-250);
+            const hasMoney = player1.deltaMoney(-100);
+            assert.equal(player1.money, 600);
+            assert.equal(hasMoney, true);
+        });
 
-		it("should go bankrupt when goes below 0", function() {
-			const hasMoney1 = player1.deltaMoney(-600);
-			assert.equal(0, player1.money);
-			assert.equal(true, hasMoney1);
+        it("should go bankrupt when goes below 0", function() {
+            const hasMoney1 = player1.deltaMoney(-600);
+            assert.equal(player1.money, 0);
+            assert.equal(hasMoney1, true);
 
-			const hasMoney2 = player1.deltaMoney(-1);
-			assert.equal(-1, player1.money);
-			assert.equal(false, hasMoney2);
+            const hasMoney2 = player1.deltaMoney(-1);
+            assert.equal(player1.money, -1);
+            assert.equal(hasMoney2, false);
 
-			const hasMoney3 = player1.deltaMoney(2);
-			assert.equal(1, player1.money);
-			assert.equal(true, hasMoney3);
-		});
-	});
+            const hasMoney3 = player1.deltaMoney(2);
+            assert.equal(player1.money, 1);
+            assert.equal(hasMoney3, true);
 
-	describe("#busPassTests", function() {
-		it("should add all of the bus passes", function() {
-			const expected = {"forward any": 2, "derp 2": 1, "back 3": 1}
-			player1.gainBusPass('forward any');
-			player1.gainBusPass('derp 2');
-			player1.gainBusPass('back 3');
-			player1.gainBusPass('forward any');
-			assert.deepEqual(expected, player1.busTickets)
-		});
+            assert(player1.canAfford(1));
+            assert(!player1.canAfford(2));
+        });
+    });
 
-		it("should remove all of the bus passes", function() {
-			const expected = {}
-			player1.useBusPass('forward any');
-			player1.useBusPass('derp 2');
-			player1.useBusPass('back 3');
-			player1.useBusPass('forward any');
-			assert.deepEqual(expected, player1.busTickets)
-		});
+    describe("#busPassTests", function() {
+        it("should add all of the bus passes", function() {
+            const expected = {"forward any": 2, "derp 2": 1, "back 3": 1}
+            player1.gainBusPass('forward any');
+            player1.gainBusPass('derp 2');
+            player1.gainBusPass('back 3');
+            player1.gainBusPass('forward any');
+            assert.deepEqual(player1.busTickets, expected);
+        });
 
-		it("should add and remove the bus passes", function() {
-			const expected = {"forward any": 1, "derp 2": 1}
-			player1.gainBusPass('forward any');
-			player1.gainBusPass('derp 2');
-			player1.gainBusPass('back 3');
-			player1.gainBusPass('forward any');
-			player1.useBusPass('back 3');
-			player1.useBusPass('forward any');
-			assert.deepEqual(expected, player1.busTickets)
-		});
+        it("should remove all of the bus passes", function() {
+            const expected = {}
+            player1.useBusPass('forward any');
+            player1.useBusPass('derp 2');
+            player1.useBusPass('back 3');
+            player1.useBusPass('forward any');
+            assert.deepEqual(player1.busTickets, expected);
+        });
 
-		it("should have all of the tickets expire", function() {
-			const expected = {"forward expire": 1}
-			player1.gainBusPass('forward any');
-			player1.gainBusPass('derp 2');
-			player1.gainBusPass('back 3');
-			player1.gainBusPass('forward any');
-			player1.useBusPass('back 3');
-			player1.gainBusPass('forward expire');
-			assert.deepEqual(expected, player1.busTickets);
-			player1.gainBusPass('forward expire');
-			assert.deepEqual(expected, player1.busTickets)
-		});
-	});
+        it("should add and remove the bus passes", function() {
+            const expected = {"forward any": 1, "derp 2": 1}
+            player1.gainBusPass('forward any');
+            player1.gainBusPass('derp 2');
+            player1.gainBusPass('back 3');
+            player1.gainBusPass('forward any');
+            player1.useBusPass('back 3');
+            player1.useBusPass('forward any');
+            assert.deepEqual(player1.busTickets, expected);
+        });
 
-	describe("#specialCardTests", function() {
-		it("should add all of the special cards", function() {
-			player1.gainSpecialCard('just say no');
-			player1.gainSpecialCard('get out of jail free');
-			player1.gainSpecialCard('steal');
-			player1.gainSpecialCard('just say no');
-			assert(player1.specialCards.indexOf('just say no') > -1);
-			assert(player1.specialCards.indexOf('steal') > -1);
-			assert(player1.specialCards.indexOf('get out of jail free') > -1);
-			assert.equal(4, player1.specialCards.length);
-		});
+        it("should have all of the tickets expire", function() {
+            const expected = {"forward expire": 1}
+            player1.gainBusPass('forward any');
+            player1.gainBusPass('derp 2');
+            player1.gainBusPass('back 3');
+            player1.gainBusPass('forward any');
+            player1.useBusPass('back 3');
+            player1.gainBusPass('forward expire');
+            assert.deepEqual(player1.busTickets, expected);
+            player1.gainBusPass('something else expire');
+            const expected2 = {"something else expire": 1}
+            assert.deepEqual(player1.busTickets, expected);
+        });
+    });
 
-		it("should remove all of the special cards", function() {
-			player1.useSpecialCard('just say no');
-			player1.useSpecialCard('get out of jail free');
-			player1.useSpecialCard('steal');
-			player1.useSpecialCard('just say no');
-			assert.equal(-1, player1.specialCards.indexOf('just say no'));
-			assert.equal(-1, player1.specialCards.indexOf('steal'));
-			assert.equal(-1, player1.specialCards.indexOf('get out of jail free'));
-			assert.equal(0, player1.specialCards.length);
-		});
-	});
+    describe("#specialCardTests", function() {
+        it("should add all of the special cards", function() {
+            player1.gainSpecialCard('just say no');
+            player1.gainSpecialCard('get out of jail free');
+            player1.gainSpecialCard('steal');
+            player1.gainSpecialCard('just say no');
+            assert(player1.specialCards.indexOf('just say no') > -1);
+            assert(player1.specialCards.indexOf('steal') > -1);
+            assert(player1.specialCards.indexOf('get out of jail free') > -1);
+            assert.equal(player1.specialCards.length, 4);
+        });
 
-	describe("#locationTest", function() {
-		it("should move the player to the location with side effects", function() {
-			let moveInfo = {"moneyGained": 299, "currentLocation": 'bob', "movedTo": []};
-			player1.moveToLocation(moveInfo);
-			assert.equal("bob", player1.location);
-			assert.equal(300, player1.money);
+        it("should remove all of the special cards", function() {
+            player1.useSpecialCard('just say no');
+            player1.useSpecialCard('get out of jail free');
+            player1.useSpecialCard('steal');
+            player1.useSpecialCard('just say no');
+            assert.equal(player1.specialCards.indexOf('just say no'), -1);
+            assert.equal(player1.specialCards.indexOf('steal'), -1);
+            assert.equal(player1.specialCards.indexOf('get out of jail free'), -1);
+            assert.equal(player1.specialCards.length, 0);
+        });
+    });
 
-			moveInfo["moneyGained"] = 0
-			moveInfo["currentLocation"] = "dude";
-			player1.moveToLocation(moveInfo);
-			assert.equal("dude", player1.location);
-			assert.equal(300, player1.money);
-		});
-	});
+    describe("#locationTest", function() {
+        it("should move the player to the location with side effects", function() {
+            let moveInfo = {"moneyGained": 299, "currentLocation": 'bob', "movedTo": []};
+            player1.moveToLocation(moveInfo);
+            assert.equal(player1.location, "bob");
+            assert.equal(player1.money, 300);
 
-	describe("#propertyTests", function() {
-		// TODO when Properties are tested to be correct, also test getNetWorth() inside this
-	});
+            moveInfo["moneyGained"] = 0
+            moveInfo["currentLocation"] = "dude";
+            player1.moveToLocation(moveInfo);
+            assert.equal(player1.location, "dude");
+            assert.equal(player1.money, 300);
+            assert.equal(player1.getNetWorth(), 300);
+        });
+    });
+
+    describe("#propertyTests", function() {
+        const p1 = new HouseProperty("boylston st", {"type": "property", "quality": "black", "rent": [30, 160, 470, 1050, 1250, 1500, 2500], "mortgage": 165, "house": 200, "forward": ["newbury st"], "side": [1], "backward": ["bonus"], "track": [0],"below": ["states ave"]});
+        const p2 = new HouseProperty("westheimer rd", {"type": "property", "quality": "light yellow", "rent": [11, 55, 160, 475, 650, 800, 1300], "mortgage": 70, "house": 100, "forward": ["internet service provider"], "backward": ["katy freeway"], "side": [1], "track": [2], "above": ["st charles pl"]})
+        const p3 = new HouseProperty("peachtree st", {"type": "property", "quality": "sea green", "rent": [20, 100, 300, 750, 925, 1100, 1600], "mortgage": 100, "house": 100, "forward": ["pay day"], "backward": ["decatur st"], "side": [1], "track": [2], "above": ["free parking"]});
+
+        p2.gainHouse();
+        p2.gainHouse();
+        p3.mortgage();
+
+        it("should gain the properties and associated wealth", function() {
+            assert.equal(player1.properties.length, 0);
+
+            player1.gainProperty(p1);
+            assert.deepEqual(player1.properties, [p1]);
+            assert.equal(player1.getNetWorth(), player1.money + p1.getValue());
+
+            player1.gainProperty(p2);
+            assert.deepEqual(player1.properties, [p1, p2]);
+            assert.equal(player1.getNetWorth(), player1.money + p1.getValue() + p2.getValue());
+
+            player1.gainProperty(p3);
+            assert.deepEqual(player1.properties, [p1, p2, p3]);
+            assert.equal(player1.getNetWorth(), player1.money + p1.getValue() + p2.getValue() + p3.getValue());
+
+            // changes propogate?
+            p3.unmortgage();
+            assert.deepEqual(player1.properties, [p1, p2, p3]);
+            assert.equal(player1.getNetWorth(), player1.money + p1.getValue() + p2.getValue() + p3.getValue());
+
+            assert.equal(player1.properties.length, 3);
+        });
+        
+        it("should lose the properties and associated wealth", function() {
+            player1.loseProperty(p1);
+            assert.deepEqual(player1.properties, [p2, p3]);
+            assert.equal(player1.getNetWorth(), player1.money + p2.getValue() + p3.getValue());
+
+            player1.loseProperty(p3);
+            assert.deepEqual(player1.properties, [p2]);
+            assert.equal(player1.getNetWorth(), player1.money + p2.getValue());
+
+            // hopefully changes in the object propogate
+            p2.removeHouse();
+            assert.deepEqual(player1.properties, [p2]);
+            assert.equal(player1.getNetWorth(), player1.money + p2.getValue());
+
+            player1.loseProperty(p2);
+            assert.deepEqual(player1.properties, []);
+            assert.equal(player1.getNetWorth(), player1.money);
+
+            assert.equal(player1.properties.length, 0);
+        });
+    });
 })
